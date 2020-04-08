c5530ca2b13dd5a33559dd54336d302543c2fc98
SAAD UMAR <sumar001@wch136-06.cs.ucr.edu>
2020-04-08 14:32:29 -0700

    checking if the new code works with gradescope tests

--- Lab2_introToAVR/turnin/sumar001_lab2_part2.c
+++ Lab2_introToAVR/turnin/sumar001_lab2_part2.c
@@ -1,7 +1,7 @@
 /*	Author: sumar001
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #2  Exercise #2
+ *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,37 +13,26 @@
 #endif
 
 int main(void) {
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs
-				   //// Initialize output on PORTC to 0x00
-     unsigned char cntavail = 0x00; 
-     unsigned char park_zero = 0x00;
-     unsigned char park_first = 0x00;
-     unsigned char park_second= 0x00;
-     unsigned char park_third = 0x00;
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0XFF ;  //Configure PORT A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0X00 ; //Configure PORT B's 8 pins as outputs, initialized to 0
 
-     while(1)
-	    {
-	      park_zero   = PINA & 0x01 ;
-	      park_first  = PINA & 0x02 ;
-	      park_second = PINA & 0x04 ;
-	      park_third  = PINA & 0x08 ;
+	unsigned char cntavail = 0x00;
+	unsigned char park_1   = 0x00;
+	unsigned char park_2   = 0x00;
+	unsigned char park_3   = 0x00;
+	unsigned char park_4   = 0x00;
 
-	     if(park_zero &  0x01) {
-		cntavail = cntavail + 1 ;
-	    }
-	if(park_first & 0x02) {
-		cntavail = cntavail + 1 ;
-	    }
-	if(park_second & 0x04) {
-		cntavail = cntavail + 1 ;
-	    }
-	if(park_third & 0x08) {
-		cntavail = cntavail + 1 ;
-	   }
-	
-	cntavail = park_zero + park_first + park_second + park_third ;
-	cntavail = 0x04 - cntavail ;
-	PORTC = cntavail;
-  }
+    /* Insert your solution below */
+    while (1) {
+		//Read input
+		park_1 = (PINA & 0x01) ? 1: 0;
+		park_2 = (PINA & 0x02) ? 1: 0;
+		park_3 = (PINA & 0x04) ? 1: 0;
+		park_4 = (PINA & 0x08) ? 1: 0;		
+
+		cntavail = 4 - (park_1 + park_2 + park_3 + park_4);
+		PORTC = cntavail;
+    }
+    return 1;
 }
