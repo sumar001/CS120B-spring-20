f68a76c18dd6b819b7f69f93b2d8b0dfce63b44c
SAAD UMAR <sumar001@wch136-06.cs.ucr.edu>
2020-04-12 12:08:01 -0700

    Updated part4 and completed the lab

--- Lab2_introToAVR/turnin/sumar001_lab2_part4.c
+++ Lab2_introToAVR/turnin/sumar001_lab2_part4.c
@@ -1,7 +1,7 @@
-/*	Author: sumar001
+/*	Author: Saad Umar
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 25
+ *	Assignment: Lab #2  Exercise #4
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -10,42 +10,47 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#include <stdlib.h>
 #endif
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0x00; PORTB = 0xFF;
-	DDRC = 0x00; PORTC = 0xFF;
-	DDRD = 0xFF; PORTD = 0x00;
-
-        unsigned char seatA = 0x00;
-	unsigned char seatB = 0x00;
-	unsigned char seatC = 0x00;
-	unsigned short totalWeight = 0x0000;
-	unsigned char differenceWeight = 0x00;
-	unsigned char tempWeight = 0x00;
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0x00; PORTB = 0x00; // Configure port B's 8 pins as inputs,
+	DDRC = 0x00; PORTC = 0x00; // Configure port C's 8 pins as inputs,
+	DDRD = 0xFF; PORTD = 0x00; // Configure port D's 8 pins as outputs,
 	
-    while(1)
-    {
-                seatA = PINA;
-		seatB = PINB;
-		seatC = PINC;
-		
-		totalWeight = seatA + seatB + seatC;
-		
-		if(abs((seatA - seatC)) >= 0x50)
-		{
-			differenceWeight = 0x02;
-		}
-		if(totalWeight >= 0x8C)
-		{
-			tempWeight = 0x01;
-		}		
-		PORTD = tempWeight | differenceWeight;
-		tempWeight = 0x00;
-		differenceWeight = 0x00;
-		totalWeight = 0x0000;
-    }
-}	
+        unsigned char weight_tot = 0x00;
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpC = 0x00; // Temporary variable to hold the value of C
+	unsigned char tmpD = 0x00; // Temporary variable to hold the value of D
+    
+    /* Insert your solution below */
+          while (1) {
+	
+		weight_tot = tmpA + tmpB + tmpC;
+		tmpA = PINA;
+		tmpB = PINB;
+		tmpC = PINC;
+		tmpD = 0x00;
+
+             
+		if(tmpA + tmpB + tmpC > 139)
+		   {
+			tmpD = tmpD | 0x01;
+		   }
+  
+		if(abs(tmpA - tmpC) > 80)
+                   {
+			tmpD = tmpD | 0x02;
+		   }  
+
+		weight_tot = weight_tot & 0xFC;
+
+		tmpD = weight_tot + tmpD;
+
+		PORTD = tmpD;
+	}
+	return 0;
+}
+
