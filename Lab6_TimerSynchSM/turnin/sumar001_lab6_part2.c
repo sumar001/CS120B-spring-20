5eed03a6a5f7750e6fbe56b03efe5ac0874a39af
sumar001 <umar.saad1998@gmail.com>
2020-05-04 22:26:03 -0700

    Working on Part 2

--- Lab6_TimerSynchSM/turnin/sumar001_lab6_part2.c
+++ Lab6_TimerSynchSM/turnin/sumar001_lab6_part2.c
@@ -1,7 +1,7 @@
 /*	Author: sumar001
  *  Partner(s) Name: 
  *	Lab Section: 25
- *	Assignment: Lab #6  Exercise #1
+ *	Assignment: Lab #6  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,7 +14,6 @@
 #include "simAVRHeader.h"
 #endif
 
-//===============Start of Lab2=====================================
 
 	unsigned char button = 0x00;
 	unsigned char tmpB = 0x00;
@@ -120,71 +119,3 @@ int main()
 }
 
 
-//=============END OF PART2=========================================
-
-/*
-//==================================================================
-//starting PART 1
-//=================================================================
-enum States {Init, Led1, Led2, Led3} state;
-
-	unsigned char tmpB = 0x00;
-
-void Tick() {
-	switch(state)
-	{
-		case Init:
-			state = Led1;
-			break;
-
-		case Led1:
-			state = Led2;
-			break;
-
-		case Led2:
-			state = Led3;
-			break;
-			
-		case Led3:
-			state = Led1;
-			break;
-	}
-
-	switch(state) {
-		case Led1:
-			tmpB = 0x01;
-			break;
-
-		case Led2:
-			tmpB = 0x02;
-			break;
-
-		case Led3:
-			tmpB = 0x04;
-			break;
-	}
-}
-
-int main(void) {
-    // Insert DDR and PORT initializations 
-	DDRB = 0XFF;   //Set PortB to output
-	PORTB = 0x00;  //Init portB to 0s
-
-	TimerSet(1000);
-	TimerOn();
-	
-	state = Init;
-
-   // Insert your solution below 
-    while (1) {
-	
-	  Tick();
-	while (!TimerFlag); //Wait 1 sec
-	TimerFlag = 0;
-	PORTB = tmpB;
-
-    }
-    return 1;
-} 
-*/
-//===================END OF PART1================================
