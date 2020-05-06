deaa47976bfdbb3b797a925cf37fb6dd3335b89f
SAAD UMAR <sumar001@wch136-06.cs.ucr.edu>
2020-05-06 16:32:08 -0700

    Working on Lab7

--- Lab7_LCD-Screen/source/main.c
+++ Lab7_LCD-Screen/source/main.c
@@ -1,23 +1,32 @@
 /*	Author: sumar001
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #7  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include "io.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+	DDRC = 0xFF; PORTC = 0x00; //LCD data Lines
+	DDRD = 0xFF; PORTD = 0x00; //LCD control lines
 
+	//Initializes the LCD display
+	 LCD_init();
+
+	//starting at position 1 on the LCD screen, writes Hello World
+	 LCD_DisplayString(1, "Hello World");
+ 
     /* Insert your solution below */
     while (1) {
-
+	continue;
     }
     return 1;
 }
