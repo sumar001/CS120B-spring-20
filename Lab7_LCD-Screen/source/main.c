b479e814cf3386226576128836644487399dcd52
SAAD UMAR <sumar001@wch136-06.cs.ucr.edu>
2020-05-06 23:08:46 -0700

    Working on p1

--- Lab7_LCD-Screen/source/main.c
+++ Lab7_LCD-Screen/source/main.c
@@ -14,7 +14,167 @@
 #include "simAVRHeader.h"
 #endif
 
-unsigned char tmpA = 0x00;
+unsigned char i = 0x00;
+unsigned char c = '0';
+
+void Tick();
+enum States {Start, INIT, INC, DEC, WAIT, RESET}state;
+
+
+int main(void)
+{
+    DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00; //LCD data lines
+	DDRD = 0xFF; PORTD = 0x00; //LCD control lines
+	
+	//Initializes the LCD display
+	LCD_init();
+	
+	TimerSet(500);
+	TimerOn();
+	
+    while(1)
+    {
+	    Tick();
+        while (!TimerFlag);
+        TimerFlag = 0;
+    }    
+}
+
+void Tick()
+{
+	switch(state) // transitions
+	{ 
+		case Start:
+		{
+			LCD_Cursor(1);
+			LCD_WriteData('0');
+			state = INIT; 
+			break;
+		}
+		
+		case INIT:
+		{
+			if((~PINA & 0x03) == 0x01)
+			{
+				state = INC; break;
+			}
+			else if((~PINA & 0x03) == 0x02)
+			{
+				state = DEC; break;
+			}
+			else if((~PINA & 0x03) == 0x03)
+			{
+				state = RESET; break;
+			}
+			else
+			{
+				state = INIT; break;
+			}
+		}
+		
+		case INC:
+		{
+			state = WAIT; break;
+		}
+		
+		case DEC:
+		{
+			state = WAIT; break;
+		}
+		
+		case WAIT:
+		{
+			if((~PINA & 0x03) == 0x01)
+			{
+				state = INC; break;
+			}
+			else if((~PINA & 0x03) == 0x02)
+			{
+				state = DEC; break;
+			}
+			else if((~PINA & 0x03) == 0x03)
+			{
+				state = RESET; break;
+			}
+			else if((~PINA & 0x03) == 0x00)
+			{
+				state = INIT; break;
+			}
+			else 
+			{
+				state = WAIT; break;
+			}
+		}
+		
+		case RESET:
+		{
+			if((~PINA & 0x03) == 0x00)
+			{
+				state = INIT; break;
+			}
+			else
+			{
+				state = RESET; break;
+			}
+		}
+		
+		default:
+			break;
+	}
+	
+	switch(state) //state actions
+	{
+		case Start:
+			break;
+			
+		case INIT:
+			break;
+			
+		case INC:
+		{
+			if(i >= 9)
+			{
+				i = 9;
+			}
+			else
+			{
+				++i;
+			}
+			LCD_Cursor(1);
+			LCD_WriteData(i + '0');
+			break;
+		}
+		
+		case DEC:
+		{
+			if(i <= 0)
+			{
+				i = 0;
+			}
+			else
+			{
+				--i;
+			}
+			LCD_Cursor(1);
+			LCD_WriteData(i + '0');
+			break;
+		}
+		
+		case WAIT:
+			break;
+			
+		case RESET:
+		{
+			i = 0; 
+			LCD_Cursor(1);
+			LCD_WriteData(i + '0');
+			break;
+		}
+	}
+}
+
+/*unsigned char tmpA = 0x00;
 unsigned char tmpC = 0x00;
 
 enum States{wait, inc, dec, zero} state;
@@ -118,7 +278,7 @@ int main(void)
 	
 	//while(1) {continue;}
 }
-/*
+
     int main(void) {
     // Insert DDR and PORT initializations 
 	DDRC = 0xFF; PORTC = 0x00; //LCD data Lines
