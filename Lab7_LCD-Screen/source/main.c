b33d3c122597dc974cf30e71bb4bbbdc999b0b73
SAAD UMAR <sumar001@wch136-06.cs.ucr.edu>
2020-05-06 17:04:06 -0700

    Working on lab7 part1

--- Lab7_LCD-Screen/source/main.c
+++ Lab7_LCD-Screen/source/main.c
@@ -9,12 +9,115 @@
  */
 #include <avr/io.h>
 #include "io.h"
+#include <timer.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+enum States{wait, inc, dec, zero} state;
+
+unsigned char tmpA = 0x00;
+unsigned char tmpC = 0x00;
+
+unsigned char button0;
+unsigned char button1;
+
+unsigned char tmpC;
+
+
+void button_Tick(){
+	button0 = ~PINA & 0x01;
+	button1 = ~PINA & 0x02;
+	
+	switch(state){ // Transitions
+		case wait:
+			if(button0 && !button1){
+				state = inc;
+			}
+			else if(!button0 && button1){
+				state = dec;
+			}
+			else if(button0 && button1){
+				state = zero;
+			}
+			else
+				state = wait;
+			break;
+		case inc:
+			if(button0 && !button1){
+				state = inc; 
+			}
+			else if(button0 && button1){
+				state = zero;
+			}
+			else
+				state = wait;
+			break;
+		case dec:
+			if(!button0 && button1){
+				state = dec;
+			}
+			else if(button0 && button1){
+				state = zero;
+			}
+			else
+				state = wait;
+			
+			break;
+		case zero:
+			if(button0 && button1){
+				state = zero;
+			}
+			else
+				state = wait;
+			break;
+	
+	}
+	switch(state){ // State actions
+		case wait:
+			break;
+		case inc:
+			if(tmpC < 9)
+				tmpC = tmpC + 1;
+			break;
+		case dec:
+			if(tmpC > 0)
+				tmpC = tmpC - 1;
+			break;
+		case zero:
+			tmpC = 0;
+			break;
+	
+	}
+}
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF; // A input initialize to 0xFF
+	DDRC = 0xFF; PORTC = 0x00; // LCD data lines
+	DDRD = 0xFF; PORTD = 0x00; // LCD control lines
+
+	TimerSet(1000);
+	TimerOn();
+	
+	// Initializes the LCD display
+	LCD_init();	
+	LCD_ClearScreen();
+	
+	state = wait;
+	tmpC = 0x00;
+	while(1){
+		LCD_Cursor(1);
+		button_Tick();
+		LCD_WriteData(tmpC + '0');
+		while(!TimerFlag){}
+		TimerFlag = 0;
+	}
+	
+}
+/*
+    int main(void) {
+    // Insert DDR and PORT initializations 
 	DDRC = 0xFF; PORTC = 0x00; //LCD data Lines
 	DDRD = 0xFF; PORTD = 0x00; //LCD control lines
 
@@ -24,9 +127,10 @@ int main(void) {
 	//starting at position 1 on the LCD screen, writes Hello World
 	 LCD_DisplayString(1, "Hello World");
  
-    /* Insert your solution below */
+    // Insert your solution below
     while (1) {
 	continue;
     }
     return 1;
 }
+*/
