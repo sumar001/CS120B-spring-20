edb08b37e5526bb8aa03f5caf7959bc7b8d6cdaa
SAAD UMAR <sumar001@wch136-06.cs.ucr.edu>
2020-05-09 05:31:09 -0700

    testing  part3

--- Lab6_TimerSynchSM/turnin/sumar001_lab6_part3.c
+++ Lab6_TimerSynchSM/turnin/sumar001_lab6_part3.c
@@ -16,241 +16,128 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States {Start, INIT, INC, DEC, WAIT, RESET}state;
+	#define A0 (~PINA & 0x01)
+	#define A1 (~PINA & 0x02)
 
-void Tick(){
-	switch(state){ //Transitions
-		case Start:
-		{
-			state = INIT;
-			break;
-		}
-		
-		case INIT:
-		if((~PINA & 0x03) == 0x01)
-		{
-			state = INC; break;
-		}
-		else if((~PINA & 0x03) == 0x02)
-		{
-			state = DEC; break;
-		}
-		else if((~PINA & 0x03) == 0x03)
-		{
-			state = RESET; break;
-		}
-		else
-		{
-			state = INIT; break;
-		}
-		
-		case INC:
-		state = WAIT;
-		break;
-		
-		case DEC:
-		state = WAIT;
-		break;
-		
-		case WAIT:
-		/*
-		if(((~PINA & 0x03) == 0x01) || ((~PINA & 0x03) == 0x02))
-		{
-			state = WAIT; break;
-		}
-		*/
-		if((~PINA & 0x03) == 0x01){
-			state = DEC;break;
-		}
-		else if((~PINA & 0x03) == 0x02){
-			state = INC;break;
-		}
-		else if((~PINA & 0x03) == 0x03)
-		{
-			state = RESET; break;
-		}
-		else if ((~PINA & 0x03) == 0x00){
-			state = WAIT; break;
-		}
-		else
-		{
-			state = INIT; break;
-		}
-		
-		case RESET:
-		if(((~PINA & 0x03) == 0x01) || ((~PINA & 0x03) == 0x02))
-		{
-			state = RESET; break;
-		}
-		else
-		{
-			state = INIT; break;
-		}
-		
-		default:
-		break;
-	}
-	switch(state){ //State actions
-		case Start:
-		{
-			PORTB = 0x07;
-		}
-		break;
-		
-		case INIT:
-		break;
-		
-		case INC:
-		{
-			if(PORTB >= 0x09)
-			{
-				PORTB = 0x09; break;
-			}
-			else
-			{
-				PORTB = PORTB + 0x01; break;
-			}
-		}
-		
-		case DEC:
-		{
-			if(PORTB <= 0x00)
-			{
-				PORTB = 0x00; break;
-			}
-			else
-			{
-				PORTB = PORTB - 0x01; break;
-			}
-		}
-		
-		case WAIT:
-		break;
-		
-		case RESET:
-		{
-			PORTB = 0x00; break;
-		}
-	}
-}
-
-int main(void)
-{
-	DDRA = 0x00;PORTA = 0xFF;
-	DDRB = 0xFF;PORTB = 0x07;
-	TimerSet(10);
-	TimerOn();
-	state = Start;
-	while(1) {
-		Tick();
-		while (!TimerFlag);
-		TimerFlag = 0;
-		// Note: For the above a better style would use a synchSM with TickSM()
-		// This example just illustrates the use of the ISR and flag
-	}
-}
+enum states{start, init, inc, dec, reset, click} state;
 
 
+	unsigned char B = 0x00;
+	unsigned char counter = 0;
 
-/*enum states{start, init, inc, dec, reset} state;
+void Tick() {
+	switch(state) {
 
-	unsigned char A0; //button A0
-	unsigned char A1; //button A1
-	unsigned char tmpB; //hold temporary value of portC
-
-
-void Tick(){
-	A0 = ~PINA & 0x01;
-	A1 = ~PINA & 0x02;
-	
-	switch(state){ // Transitions
 		case start:
+			state = init;
+			break;
+
+		case init:
+			if(A0 && !A1) {
+				state = inc;
+				}
+			else if(!A0 && A1) {
+				state = dec;
+				}
+			else if(A0 && A1) {
+				state = reset;
+				}
+			else {
 				state = init;
+				}
 				break;
 
-		case init:
-			if(!A0 && A1){
-				state = dec;
-			}
-			else if(A0 && !A1){
-				state = inc;
-			}
-			else if(A0 && A1){
-				state = reset;
-			}
-			else
-				state = init;
-			break;
-
 		case inc:
-			if(A0 && !A1){
-				state = inc; 
-			}
-			else if(A0 && A1){
-				state = reset;
-			}
-			else
-				state = init;
+			state = click;
 			break;
 
 		case dec:
-			if(!A0 && A1){
-				state = dec;
-			}
-			else if(A0 && A1){
-				state = reset;
-			}
-			else
+			state = click;
+			 break;
+
+		case reset:
+			state = init;
+			break;
+
+		case click:
+			if(A0 && !A1) {
+				state = click;
+				if(counter >= 10)
+					{
+					 state = init;
+					 counter = 0;
+					}
+				}
+
+			else if(!A0 && A1) {
+				state = click;
+				if(counter >= 10) {
+					state = init;
+					counter = 0;
+					}
+				}
+
+			else {
 				state = init;
+			    }
+				break;
+
+		default:
+			break;
+	    }
+
+	switch(state) {
+		case start:
+			break;
+
+		 case init:
+			break;
+		
+		case inc:
+			if( B < 0x09) {
+				B++;
+			  }
+
+		 case dec:
+			if(B > 0x09) {
+				B-- ;
+			 }
+
+		 case reset:
+			B = 0x00;
+			break;
+
+		 case click:
 			
+			counter++;
 			break;
 
-		case reset:
-			if(A0 && A1){
-				state = reset;
-			}
-			else
-				state = init;
+		default:
 			break;
-	
+		}
 	}
-	switch(state){ // State actions
-		case init:
-			break;
 
-		case inc:
-			if(tmpB < 9)
-				tmpB++ ;
-			break;
+int main(void) {
+ DDRA = 0x00; PORTA = 0xFF;
+ DDRB = 0xFF; PORTB = 0x00;
 
-		case dec:
-			if(tmpB > 0)
-				tmpB--;
-			break;
+ B = 0x07;
+ state = start;
 
-		case reset:
-			tmpB = 0;
-			break;
-	
-	}
-}
+ TimerSet(100);
+ TimerOn();
 
-int main(void)
-{
-	DDRA = 0x00; PORTA = 0xFF; 
-	DDRB = 0xFF; PORTB = 0x00; 
+ while(1) {
+	Tick();
+        PORTB = B;
 
-	TimerSet(100);
-	TimerOn();
-	
-	state = start;
-	tmpB = 0x07;
-	
-	while(1){
-		Tick();
-		while(!TimerFlag){}
+	while(!TimerFlag) {}
 		TimerFlag = 0;
-		PORTB = tmpB;
 	}
+   return 1;
 }
-*/
+
+ 
+
+
+
