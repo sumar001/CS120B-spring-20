5fdd1c5a3ad8e95c14f37ec70068ee0943dc1bd1
sumar001 <umar.saad1998@gmail.com>
2020-05-08 17:28:16 -0700

    Completed Lab7

--- Lab7_LCD-Screen/turnin/sumar001_lab7_part2.c
+++ Lab7_LCD-Screen/turnin/sumar001_lab7_part2.c
@@ -14,100 +14,112 @@
 #include "simAVRHeader.h"
 #endif
 
-unsigned char tmpA = 0x00;
-  unsigned char tmpC = 0x00;
-  unsigned char score = 0x00;
-  unsigned char i = 0x00;
+  unsigned char A = 0x00; //button
+  unsigned char tmpB = 0x00;
+  unsigned char result = 0x00;
+  unsigned char i = 0x00; //counter
 
 
-  enum States{start, light1, wait1, light2, wait2, light3, wait3}state;
+enum States{start, led1, led2, led3, hold1, hold2, hold3}state;
 
-void lightTick(){
+void Tick(){
 	switch (state) // Transitions
 	{
 		case start:
-			state = light1;
+			state = led1;
 			break;
 
-		case light1:
-			if(tmpA){
-				state = wait1;
-				if(score > 0)
-					score = score -1;
+		case led1:
+			if(A){
+				state = hold1;
+				if(result > 0)
+					result--;
 			}
 			else{
-				state = light2;
+				state = led2;
 			}
 			break;
-		case wait1:
-			if(tmpA){
-				state = light1;
+
+		case hold1:
+			if(A){
+				state = led1;
 			}
 			else{
-				state = wait1;
+				state = hold1;
 			}
 			break;
-		case light2:
-			if(tmpA){
-				state = wait2;
-				if(score < 9)
-					score = score + 1;
+
+		case led2:
+			if(A){
+				state = hold2;
+				if(result < 9)
+					result++;
 			}
+
 			else if(i % 2 == 0){
-				state = light1;
+				state = led1;
 				i++;
 			}
 
 			else{
-				state = light3;
+				state = led3;
 			}
 			break;
-		case wait2:
-			if(tmpA){
-				state = light1;
+
+		case hold2:
+			if(A){
+				state = led1;
 			}
 			else{
-				state = wait2;
+				state = hold2;
 			}
 			break;
-		case light3:
-			if(tmpA){
-				state = wait3;
-				if(score > 0)
-					score = score - 1;
+
+		case led3:
+			if(A){
+				state = hold3;
+				if(result > 0)
+					result--;
 			}
+
 			else{
-				state = light2;
+				state = led2;
 				i++;
 			}
 			break;
-		case wait3:
-			if(tmpA){
-				state = light1;
+
+		case hold3:
+			if(A){
+				state = led1;
 			}
 			else{
-				state = wait3;
+				state = hold3;
 			}
 			break;
 	}
 	switch (state){ // State actions
-		case light1:
-			tmpC = 0x01;
+		case led1:
+			tmpB = 0x01;
 			break;
-		case wait1:
-			tmpC = 0x01;
+
+		case hold1:
+			tmpB = 0x01;
 			break;
-		case light2:
-			tmpC = 0x02;
+
+		case led2:
+			tmpB = 0x02;
 			break;
-		case wait2:
-			tmpC = 0x02;
+
+		case hold2:
+			tmpB = 0x02;
 			break;
-		case light3:
-			tmpC = 0x04;
+
+		case led3:
+			tmpB = 0x04;
 			break;
-		case wait3:
-			tmpC = 0x04;
+
+		case hold3:
+			tmpB = 0x04;
 			break;
 
 	}
@@ -116,34 +128,37 @@ void lightTick(){
 
   int main()
   {
-	  DDRB = 0xFF; PORTB = 0x00; // Initiate DDRC to outputs
-	  DDRA = 0x00; PORTA = 0xFF; // Initiate DDRA to inputs
+	  DDRA = 0x00; PORTA = 0xFF; // DDRA for input Button
+	  DDRB = 0xFF; PORTB = 0x00; // DDRB for Led
 	  DDRC = 0xFF; PORTC = 0x00; // LCD data lines
 	  DDRD = 0xFF; PORTD = 0x00; // LCD control lines
 
 	  TimerSet(300);
 	  TimerOn();
+
 	  state = start;
 
 	  // Initialize LCD display
 	  LCD_init();
 	  LCD_ClearScreen();
-	  score = 0x05;
+	  result = 0x05;
 
 	  while(1) {
 		LCD_Cursor(1);
-		if(score == 9)
-			LCD_DisplayString(1,"You Win!!");
-		else
-			LCD_WriteData(score + '0');
-		tmpA = ~PINA & 0x01;
-		// User code (i.e. synchSM calls)
-		lightTick();
-		while (!TimerFlag){}	// Wait 1 sec
+
+		if(result == 9) {
+			LCD_DisplayString(1,"Winner!!");
+		}
+		else {
+			LCD_WriteData(result + '0');
+		}
+
+		 A = ~PINA & 0x01;
+		
+		Tick();
+		while (!TimerFlag){}
 		TimerFlag = 0;
-		// Note: For the above a better style would use a synchSM with TickSM()
-		// This example just illustrates the use of the ISR and flag
-		PORTB = tmpC;
+		PORTB = tmpB;
 		  
 	  }
 	  return 0;
