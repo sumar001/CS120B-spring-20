fe3a6f985fa980dc238b55c663a9b6bba403d086
SAAD UMAR <sumar001@wch136-06.cs.ucr.edu>
2020-05-13 15:41:31 -0700

    Working on part1

--- Lab8_ADC/source/main.c
+++ Lab8_ADC/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: sumar001
  *  Partner(s) Name: 
  *	Lab Section: 25
- *	Assignment: Lab #8  Exercise #1
+ *	Assignment: Lab #8  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -22,21 +22,20 @@ void ADC_init() {
 	//        the previous conversion completes.
 }
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF; // Set A to input and initialize to 0xFF
-	DDRB = 0xFF; PORTB = 0x00; // Set B to output and initialize to 0x00
-	DDRD = 0xFF; PORTD = 0x00; // Set D to output and initialize to 0x00
+int main(void)
+{
+	DDRA = 0x00;PORTA = 0xFF;
+	DDRB = 0xFF;PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
 
 	ADC_init();
+
+	/*Insert your solution below */
+	while(1){
 	
-    /* Insert your solution below */
-    while (1) {
-	unsigned short x = ADC; //Value of ADC register is now stored in variable x
-		unsigned char first8 = (char)x;
-		PORTB = first8;
-		unsigned char last2 = (char)(x >> 8);
-		PORTD = last2;
-    }
-    return 1;
+	unsigned short x = ADC; //Storing value of ADC
+	PORTB = (char)x;
+	PORTD = (char)(x >> 8);
+	
+	}
 }
