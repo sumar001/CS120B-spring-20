549791b17469c891c72cb0938756f804c45be0ba
SAAD UMAR <sumar001@wch136-06.cs.ucr.edu>
2020-05-17 16:43:35 -0700

    Completed Lab9 with demo links

--- Lab9_PWM/turnin/sumar001_lab9_part2.c
+++ Lab9_PWM/turnin/sumar001_lab9_part2.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	Demo: https://drive.google.com/file/d/1G1bF47qcrdDouykFa_-YMcDemG9QQetN/view?usp=sharing 
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
--- Lab9_PWM/turnin/sumar001_lab9_part3.c
+++ Lab9_PWM/turnin/sumar001_lab9_part3.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	Demo: https://drive.google.com/file/d/1rvhUHJgCV6QBA4Xg9R7CkDjM3FLaeTH7/view?usp=sharing
  */
 #include <avr/io.h>
 #include <timer.h>
