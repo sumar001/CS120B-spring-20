cd0e87565e1e951bd277c3808804de75aa4483e7
SAAD UMAR <sumar001@wch136-06.cs.ucr.edu>
2020-06-07 18:37:26 -0700

    Workingg on part 5

--- Lab11_TaskScheduler/source/main.c
+++ Lab11_TaskScheduler/source/main.c
@@ -20,90 +20,186 @@
 #include "io.h"
 //#include "keypad.h"
 #include "scheduler.h"
+ 
+/*	Partner(s) Name & E-mail: Jeff Trang (jtran094@ucr.edu), Kevin Tsai(ktsai017@ucr.edu), Xiangyu Chang(3750627@qq.com)
+ *	Lab Section: 022
+ *	Assignment: Lab #11  Exercise #5 
+ *	Exercise Description: design a game where the player avoids obstacles on the LCD screen
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
 
-enum SMTask1_States {start} state;
-int Tick_keypad(int state) {
-	unsigned char x;
-	x = GetKeypadKey();
-	switch(state) {
-		case start:
-		   switch(x) {
-			case '\0' : PORTB = 0x1F; break; //All 5 Leds on
-			case '1' : PORTB = 0x01; break;
-			case '2' : PORTB = 0x02; break;
-			case '3' : PORTB = 0x03; break;
-			case '4' : PORTB = 0x04; break;
-			case '5' : PORTB = 0x05; break;
-			case '6' : PORTB = 0x06; break;
-			case '7' : PORTB = 0x07; break;
-			case '8' : PORTB = 0x08; break;
-			case '9' : PORTB = 0x09; break;
-			case 'A' : PORTB = 0x0A; break;
-			case 'B' : PORTB = 0x0B; break;
-			case 'C' : PORTB = 0x0C; break;
-			case 'D' : PORTB = 0x0D; break;
-			case '*' : PORTB = 0x0E; break;
-			case '0' : PORTB = 0x00; break;
-			case '#' : PORTB = 0x0F; break;
-			default: PORTB = 0x1B; break;
-			}
-		state = start;
-		break;
-	     }
-	return state;
-       }
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "timer.h"
+#include "io.c"
 
+// global variables
+unsigned char tmpB;
+unsigned char lcd_top;
+unsigned char lcd_bot;
+unsigned char user_position; // 1 = top, 17 = bottom
+unsigned char obj_top_position;
+unsigned char obj_bot_position;
+int paused;
+
+// state machines
+enum cursor_States {cursor_start, up, down} cursor_state;
+enum obj_States {obj_start, move_obj} obj_state;
+enum game_States {game_start, play, lose, wait, reset} game_state;
+
+void move_cursor() {
+    switch(cursor_state) { // state transitions
+        case cursor_start:
+            cursor_state = up;
+            break;
+        case up:
+            if (tmpB == 0x02) {
+                cursor_state = down;
+            }
+            break;
+        case down:
+            if (tmpB == 0x01) {
+                cursor_state = up;
+            }
+            break;
+    }
+    switch(cursor_state) { // state actions
+        case cursor_start:
+            break;
+        case up:
+            LCD_Cursor(1);
+            user_position = 1;
+            break;
+        case down:
+            LCD_Cursor(17);
+            user_position = 17;
+            break;
+    }
+}
+
+void create_obstacles() {
+    switch(obj_state) { // state transitions
+        case obj_start:
+            obj_state = move_obj;
+            break;
+        case move_obj:
+            if (lcd_top != 1 && lcd_bot != 17) {
+                lcd_top = lcd_top - 1;
+                lcd_bot = lcd_bot - 1;
+            }
+            else if (lcd_top == 1) {
+                lcd_top = 16;
+            }
+            else if (lcd_bot == 17) {
+                lcd_bot = 32;
+            }
+            break;
+    }
+    switch(obj_state) { // state actions
+        case obj_start:
+            break;
+        case move_obj:
+            LCD_ClearScreen();
+            LCD_DisplayString(lcd_top, (unsigned char*)"?");
+            obj_top_position = lcd_top;
+            LCD_Cursor(lcd_bot); // NOTE: problems with using DisplayString() or WriteData() twice
+            LCD_WriteData('?');  // top LCD doesn't display '?' properly; temporary fix
+            obj_bot_position = lcd_bot;
+            break;
+    }
+}
+
+void game_function() {
+    switch(game_state) { // state transitions
+        case game_start:
+            game_state = wait;
+            break;
+        case play:
+            if (user_position == obj_top_position || user_position == obj_bot_position) {
+                game_state = lose;
+            }
+            break;
+        case lose:
+            game_state = wait;               
+            break;
+        case wait:
+            if (tmpB == 0x04) {
+                game_state = reset;
+            }
+            break;
+        case reset:
+            game_state = play;
+            break;
+    }
+    switch(game_state) { // state actions
+        case game_start:
+            break;
+        case play:
+            move_cursor();
+            break;
+        case lose:
+            LCD_DisplayString(1, (unsigned char*)"Just dodge them. 4Head");
+            paused = 1;
+            break;
+        case wait:
+            break;
+        case reset:
+            LCD_ClearScreen();
+            paused = 0;
+            break;
+    }
+}
 
 int main(void) {
-   	unsigned char x;
-     /* Insert DDR and PORT initializations */
-	DDRB = 0xFF; PORTB = 0x00;
-	DDRC = 0xF0; PORTC = 0x0F;
-
-    /* Insert your solution below */
-	 unsigned long int Tick_keypad_calc = 20;
-
-	//Calculating GCD
-	unsigned long int tmpGCD = 10;
-
-	//Greatest common divisor for all tasks or smallest time unit for tasks.
-	unsigned long int GCD = tmpGCD;
-
-	//Recalculate GCD periods for scheduler
-	unsigned long int Tick_keypad_period = Tick_keypad_calc/GCD;
-
-	//Declare an array of tasks
-	static task task1;
-	task *tasks[] = { &task1};
-	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
-
-	// Task 1
-	task1.state = 0;//Task initial state.
-	task1.period = Tick_keypad_period;//Task Period.
-	task1.elapsedTime = Tick_keypad_period;//Task current elapsed time.
-	task1.TickFct = &Tick_keypad;//Function pointer for the tick.
-
-
-	// Set the timer and turn it on
-	TimerSet(GCD);
-	TimerOn();
-
-	unsigned short i; // Scheduler for-loop iterator
-	while(1) {
-		// Scheduler code
-		for ( i = 0; i < numTasks; i++ ) {
-			// Task is ready to tick
-			if ( tasks[i]->elapsedTime == tasks[i]->period ) {
-				// Setting next state for task
-				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
-				// Reset the elapsed time for next tick.
-				tasks[i]->elapsedTime = 0;
-			}
-			tasks[i]->elapsedTime += 1;
-		}
-		while(!TimerFlag);
-		TimerFlag = 0;
-	}
+    // initialize ports
+    DDRB = 0x00; PORTB = 0xFF;
+    DDRA = 0xFF; PORTA = 0x00; // LCD control lines
+    DDRD = 0xFF; PORTD = 0x00; // LCD data lines
+    
+    // initialize global variables
+    tmpB = 0x00;
+    lcd_top = 15; // random position on top LCD
+    lcd_bot = 26; // random position on bottom LCD
+    paused = 1;
+    // initialize local variables
+    unsigned long cursor_time = 50;
+    unsigned long obstacle_time = 300;
+    const unsigned long timer_period = 50;
+    
+    // initialize states
+    cursor_state = cursor_start;
+    obj_state = obj_start;
+    game_state = game_start;
+    
+    // initialize timer
+    TimerSet(timer_period);
+    TimerOn();
+    LCD_init();
+    while (1) {
+        tmpB = ~PINB & 0x07; // use only PB0-PB2
+        if (tmpB == 0x04) { // use PB2 to start/restart the game
+            paused = 0;
+        }
+        if (paused == 0) {
+            if (cursor_time >= 50) {
+                game_function();
+                cursor_time = 0;
+            }
+            if (obstacle_time >= 300) {
+                create_obstacles();
+                obstacle_time = 0;
+            }
+        }        
+        while (!TimerFlag) {}  // Wait for timer period
+        TimerFlag = 0;        // Lower flag
+        cursor_time += timer_period;
+        obstacle_time += timer_period;        
+    }
 }
-	
-"
+
+
+
+
+
